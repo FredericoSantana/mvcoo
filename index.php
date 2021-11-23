792c6a08b131b0a57ed81128b83744ed6f4ab19b
FredericoSantana <fredericosantana@hotmail.com.br>
2021-11-23 15:30:13 -0300

    Camada de manipulação: helpers-string-2

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 require_once __DIR__ . '/source/autoload.php';
 
+$string = "Essa é uma string, nela temos um under_score e um guarda-chuva";
+$message = new \Source\Core\Message();
+echo $message->info(str_slug($string));
+echo $message->info(str_studly_case($string));
+echo $message->info(str_camel_case($string));
+echo $message->info(str_title($string));
+echo $message->info(str_limit_words($string, 10));
+echo $message->info(str_limit_chars($string, 48));
