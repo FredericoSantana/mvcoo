f59d348b1bf712aeb0b9f66b3f5fa1d43163c48e
FredericoSantana <fredericosantana@hotmail.com.br>
2021-11-24 10:09:18 -0300

    Verificando password com hash

--- index.php
+++ index.php
@@ -1,32 +1,12 @@
 <?php
 require_once __DIR__ . '/source/autoload.php';
 
-$post = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
-if ($post) {
-  $data = (object)$post;
+$user = (new \Source\Models\User())->findById(1);
+//$user->password = 12345678;
+$user->save();
 
-  if (!csrf_verify($post)){
-    $error = message()->error("Erro ao enviar, favor tente novamente");
-  }else{
-    $user = new \Source\Models\User();
-    $user->boostrap(
-      $data->first_name,
-      $data->last_name,
-      $data->email,
-      $data->password
-    );
-
-    if (!$user->save()) {
-      echo $user->message();
-    }else{
-      echo message()->success("Cadastro realizado com sucesso");
-//      unset($data);
-    }
-
-    var_dump($user->data());
-  }
-
-  var_dump($data);
-}
-
-require 'form.php';
+var_dump(
+  $user,
+  password_get_info(12345678),
+  password_get_info(passwd(12345678))
+);
