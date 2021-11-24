33f38f01da0f7f1a751e0e117916640cd1568a9f
FredericoSantana <fredericosantana@hotmail.com.br>
2021-11-24 10:19:06 -0300

    Consulta em palavras reservadas

--- index.php
+++ index.php
@@ -2,11 +2,8 @@
 require_once __DIR__ . '/source/autoload.php';
 
 $user = (new \Source\Models\User())->findById(1);
-//$user->password = 12345678;
 $user->save();
 
 var_dump(
-  $user,
-  password_get_info(12345678),
-  password_get_info(passwd(12345678))
+  $user
 );
